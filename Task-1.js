dd1a9c393dd3732179f6677c65eb9b16f76021db
Aliaksei Bakhur <aliaksei_bakhur@epam.com>
2018-05-02 18:42:28 +0300

    Task-1: new parameter numberOfValues added to getMultiplyGreatestValues; general code improbement to the method

--- Task-1.js
+++ Task-1.js
@@ -3,31 +3,33 @@
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min; // generates Random integer from [min, max]
-} 
-  
+}
+
 function getArray(n) {
-  var array = [];
-	for (var i = 0; i < n; i++) {
+  let array = [];
+	for (let i = 0; i < n; i++) {
 	  array.push(getRandomInteger(0, 10));
 	}
 	return array;
 }
-  
+
 function compareNumeric(a, b) {
-  return a - b;
-}
-  
-function getMultiplyOfThreeGreatestValues(array) {
-  if ( array.length >= 3 ) {
-    var sortedArray = array.slice().sort(compareNumeric);
-	  return sortedArray[sortedArray.length - 1] * 
-           sortedArray[sortedArray.length - 2] *
-			     sortedArray[sortedArray.length - 3];
-	}
-	return "Provided array has less then 3 items";
+  return b - a;
 }
 
-var itemsInArray = 10;
-var myArray = getArray(itemsInArray);
+function getMultiplyGreatestValues(array, numberOfValues) {
+	if ( array.length < numberOfValues ) {
+		return '\n Error: provided array has less then ' + numberOfValues + ' items. Please change "itemsInArray" parameter';
+	}
+	let sortedArray = array.slice().sort(compareNumeric).slice(0, numberOfValues); // create clone of array, sort it and get the biggest values
+	let result = sortedArray.reduce(function(multiply, current) {
+		return multiply * current;
+	}, 1);
+	return result
+}
+
+const itemsInArray = 10;
+const numberOfValues = 3;
+let myArray = getArray(itemsInArray);
 console.log('Generated array with', itemsInArray, 'random integer items:\n', myArray);
-console.log( 'Multiply of 3 greatest values from the array: ', getMultiplyOfThreeGreatestValues(myArray) );
+console.log( 'Multiply of', numberOfValues, 'greatest values from the array: ', getMultiplyGreatestValues(myArray, numberOfValues) );
