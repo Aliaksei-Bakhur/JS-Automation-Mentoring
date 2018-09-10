17c7d8352278a561ef9f105553c86aee61292845
Aliaksei Bakhur <aliaksei_bakhur@epam.com>
2018-09-10 21:31:14 +0300

    Updated BaseUrl due to implementation of saml-idp and okta logins

--- Tasks/M8 - M10/webdriverIO/test/config/suite.cucumber.conf.js
+++ Tasks/M8 - M10/webdriverIO/test/config/suite.cucumber.conf.js
@@ -117,7 +117,7 @@ exports.config = {
   //
   // Set a base URL in order to shorten url command calls. If your url parameter starts
   // with "/", then the base url gets prepended.
-  baseUrl: 'http://localhost:7000/',
+  baseUrl: 'https://qa-auto-01.elateral-dev.io/signin',
   waitforTimeout: 90000,            // Default timeout for all waitFor* commands.
   connectionRetryTimeout: 90000,    // Default timeout in milliseconds for request  if Selenium Grid doesn't send response
   connectionRetryCount: 3,          // Default request retries count
