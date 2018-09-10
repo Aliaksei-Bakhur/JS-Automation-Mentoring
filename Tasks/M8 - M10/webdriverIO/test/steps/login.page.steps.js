b471b504922ea70709d32984db073d92d3c8cd50
Aliaksei Bakhur <aliaksei_bakhur@epam.com>
2018-09-10 21:34:38 +0300

    Added loginMethod parameter to Login step

--- Tasks/M8 - M10/webdriverIO/test/steps/login.page.steps.js
+++ Tasks/M8 - M10/webdriverIO/test/steps/login.page.steps.js
@@ -1,20 +1,14 @@
 /* global browser,expect,assert */
 import { defineSupportCode } from 'cucumber';
-const creds = require('../config/creds');
 const { config } = require('../config/suite.cucumber.conf');
 const loginFactory = require('../helpers/loginFactory');
 
 defineSupportCode(({ Given, Then, When }) => {
-  When(/^I login to Portal via saml-idp$/, () => {
+  When(/^I login to Portal via "(.*)"$/, (loginMethod) => {
     browser.url(config.baseUrl);
-    const saml = loginFactory('saml-idp');
-    saml.waitPageReady();
-    saml.userNameInput.setValue(creds.userName);
-    saml.firstNameInput.setValue(creds.firstName);
-    saml.lastNameInput.setValue(creds.lastName);
-    saml.emailInput.setValue(creds.email);
-    saml.roleSelect.selectByVisibleText(creds.role);
-    saml.signInButton.click();
+    browser.pause(2000);
+    const login = loginFactory(loginMethod);
+    login.loginToPortal();
   });
 
 });
