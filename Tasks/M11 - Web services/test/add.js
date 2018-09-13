28f48452aa789e308aec0a36813bced0d9826ed6
Aliaksei Bakhur <aliaksei_bakhur@epam.com>
2018-09-13 14:47:51 +0300

    Corrected names (files, variables)

--- Tasks/M11 - Web services/test/add.js
+++ Tasks/M11 - Web services/test/add.js
@@ -1,30 +1,30 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sendRequest = require('../lib/sendRequest');
-const getComment = require('../data/getComment');
+const getUsers = require('../data/getUsers');
 const env = require('../endpoint/test');
 
-describe('Get Comment by id', () => {
+describe('Get users', () => {
 
-    getComment.map((data) => {
-        let response;
+  getUsers.map((data) => {
+    let response;
 
-        before(async () => {
-            data.uri = env.uri + data.uri;
-            response = await sendRequest(data);
-        });
+    before(async () => {
+      data.uri = env.uri + data.uri;
+      response = await sendRequest(data);
+    });
 
-        it('Verifying response code is 200', () => {
-            expect(response.statusCode).to.eql(200);
-        });
+    it('Verifying response code is 200', () => {
+      expect(response.statusCode).to.eql(200);
+    });
 
-        it('Verifying content-type header exists and equals to application/json; charset=utf-8', () => {
-            expect(response.headers['content-type']).to.eql('application/json; charset=utf-8');
-        });
+    it('Verifying content-type header exists and equals to application/json; charset=utf-8', () => {
+      expect(response.headers['content-type']).to.eql('application/json; charset=utf-8');
+    });
 
-        it('Verifying the response body is the array of 10 users', () => {
-            expect(response.body.length).to.eql(10);
-        });
-    })
+    it('Verifying the response body is the array of 10 users', () => {
+      expect(response.body.length).to.eql(10);
+    });
+  })
 
 });
